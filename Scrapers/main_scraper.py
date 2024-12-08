6a7354dfe30c1d90dbd02f631597f1571211cde5
jihodude <66758210+jihodude@users.noreply.github.com>
2024-12-07 18:46:51 -0800

    initialization, setup the project structure
    used python-decouple, .env and .gitignore to handle private data such as API Keys

---
+++ Scrapers/main_scraper.py
@@ -0,0 +1,17 @@
+# # from scrapers.youtube_scraper import YouTubeScraper
+# # from scrapers.pexels_scraper import PexelsScraper
+# # from scrapers.pixabay_scraper import PixabayScraper
+
+# class ScraperManager:
+#     def __init__(self, youtube_key, pexels_key, pixabay_key):
+#         # self.youtube = YouTubeScraper(api_key=youtube_key)
+#         # self.pexels = PexelsScraper(api_key=pexels_key)
+#         # self.pixabay = PixabayScraper(api_key=pixabay_key)
+
+#     # def scrape_all(self, query):
+#         # return {
+#         #     "videos": self.youtube.fetch_videos(query=query),
+#         #     "photos": self.pexels.fetch_photos(query=query),
+#         #     "images": self.pixabay.fetch_images(query=query),
+#         # }
+
